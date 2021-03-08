b4cfd108f932685704a65434acc0cb9029bc7de9
Ricardo Assuncao Vialle <ricardovialle@gmail.com>
2021-03-08 13:49:50 -0500

    Clean not used reference files

--- data/create_ensembl_vcf.sh
+++ data/create_ensembl_vcf.sh
@@ -6,17 +6,21 @@ ml bcftools
 # download each file
 for i in {1..22}; do 
     if [ ! -f homo_sapiens-chr${i}.vcf.gz ]; then
-    axel ftp://ftp.ensembl.org/pub/release-99/variation/vcf/homo_sapiens/homo_sapiens-chr${i}.vcf.gz
-    tabix homo_sapiens-chr${i}.vcf.gza
+    	axel ftp://ftp.ensembl.org/pub/release-99/variation/vcf/homo_sapiens/homo_sapiens-chr${i}.vcf.gz
+    	tabix homo_sapiens-chr${i}.vcf.gz
     fi
 done
 
-
 # set chr names
-for i in {1..22}; do echo $i chr$i; done > chr_name_conv.txt
+for i in {1..22}; do 
+	echo $i chr$i 
+done > chr_name_conv.txt
 
 # merge together and set chr names to chr1
-bcftools concat --threads 4  homo_sapiens-chr{1..22}.vcf.gz -Oz -o ensembl_v99_hg38_nochr.vcf.gz
+bcftools concat --threads 4 homo_sapiens-chr{1..22}.vcf.gz -Oz -o ensembl_v99_hg38_nochr.vcf.gz
 
 bcftools annotate --rename-chrs chr_name_conv.txt -Oz -o ensembl_v99_hg38.vcf.gz ensembl_v99_hg38_nochr.vcf.gz
 
+tabix -p vcf ensembl_v99_hg38.vcf.gz
+
+rm -rf homo_sapiens-chr*
